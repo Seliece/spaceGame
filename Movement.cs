b3729ae451d6bcd1686bcdae371136d4531dee90
Rens Nijman <rensnijman2@gmail.com>
2024-02-15 11:01:03 +0100

    i forgor what i did

--- Movement.cs
+++ Movement.cs
@@ -5,29 +5,38 @@ using System.Drawing;
 public partial class Movement : CharacterBody2D
 {
 	[Export] private int speed;
+	KinematicCollision2D Collision2D;
+	TileMap tileMap;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GetChild<AnimatedSprite2D>(2).Play();
+		tileMap = GetNode<TileMap>("Tilemap");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Move();
-		MoveAndSlide();
+		AddVelocity();
+		Collision2D = MoveAndCollide(Velocity);
+		CheckCollision();
 	}
 
-	public void Move() {
+	public void AddVelocity() {
 		Velocity = new Vector2(0,0);
 		if (Input.IsKeyPressed(Key.D)) {
 			 
 			Velocity += new Vector2(speed, 0); 
-			GetChild<AnimatedSprite2D>(2).Scale = new Vector2(2, 2);
+			if (!Input.IsKeyPressed(Key.Shift)) {
+				GetChild<AnimatedSprite2D>(2).Scale = new Vector2(2, 2);
+			}
 		}
 		if (Input.IsKeyPressed(Key.A)) {
-			Velocity += new Vector2(-speed, 0); 
-			GetChild<AnimatedSprite2D>(2).Scale = new Vector2(-2, 2);
+			Velocity += new Vector2(-speed, 0);
+			if (!Input.IsKeyPressed(Key.Shift)) {
+				GetChild<AnimatedSprite2D>(2).Scale = new Vector2(-2, 2);
+			}
 		}
 		if (Input.IsKeyPressed(Key.W)) {
 			Velocity += new Vector2(0, -speed); 
@@ -37,6 +46,10 @@ public partial class Movement : CharacterBody2D
 		}
 	}
 	public void CheckCollision() {
-	//	if ()
+		GodotObject collider = Collision2D.GetCollider();
+		if (collider.GetType() == TileMap) {
+			
+		}
+		
 	}
 }
